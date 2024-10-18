a23f11c441b8fd3c6d2a6befeb4a2ca18eb678a6
Carlos Beltran <carbeltor3@alu.edu.gva.es>
2024-10-18 20:45:57 +0200

    añadidas variables locales

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,6 +3,8 @@
 
 // TODO: Variable global
 let presupuesto = 0;
+let gastos ={};
+let idGasto = 0;
 
 function actualizarPresupuesto(valor) {
     // TODO
@@ -57,8 +59,10 @@ function CrearGasto(descripcion,valor) {
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
 }
+
