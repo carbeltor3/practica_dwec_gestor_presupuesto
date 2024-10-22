c31fc8b3e8be16259dddabbbe259e1c23ae0727a
Carlos Beltran <carbeltor3@alu.edu.gva.es>
2024-10-22 11:06:34 +0200

    funcion listar gastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,17 +3,16 @@
 
 // TODO: Variable global
 let presupuesto = 0;
-let gastos ={};
+let gastos =[];
 let idGasto = 0;
 
 function actualizarPresupuesto(valor) {
     // TODO
-    if(valor>=0) {
-        presupuesto = valor;
-    }else if(valor<0){
-        presupuesto = -1;
+    
+    if ((isNaN(valor))||(valor<0)){
+        return -1;
     }else{
-      presupuesto = presupuesto;
+        presupuesto = valor;
     }
     return presupuesto;
 }
@@ -22,69 +21,91 @@ function actualizarPresupuesto(valor) {
 
 function mostrarPresupuesto() {
     // TODO
-    
-    let texto;
-    texto = `Tu presupuesto actual es de ${presupuesto} €`;
-    return texto;
+    return `Tu presupuesto actual es de ${presupuesto} €`;
+     
     
 }
 
-function CrearGasto(descripcion,valor) {
+function CrearGasto(descripcion,valor,fecha,...etiquetas) {
     // TODO
-        if((typeof(valor) === "number")&& (valor>=0)){
-            this.valor =valor;
-            this.descripcion= descripcion;
+    this.descripcion = descripcion;
+        
+        if ((isNaN(valor))||(valor<0)){
+            this.valor =0;
+        }else{
+            this.valor = valor;
         }
-        else{
-            this.valor = 0;
-            this.descripcion = descripcion;
+
+        if(!fecha){
+            this.fecha = new Date().toISOString();
+        }else{
+            this.fecha = Date.parse(fecha);
         }
+
+        
+        
+        
         this.mostrarGasto = function(){
             return `Gasto correspondiente a ${descripcion} con valor ${valor} €`;
         }
+
         this.actualizarDescripcion = function(descripcion){
             this.descripcion = descripcion;
         }
+
         this.actualizarValor = function(valor){
             if(valor>=0){
                 this.valor = valor;
             }
         }
-}
 
-function listarGastos(){
-    return gastos;
-}
+        
 
-function anyadirGasto(){
+    }
 
-}
-
-function borrarGasto(){
-
-}
+    function listarGastos(){
+        return gastos;
+    }
     
-function calcularTotalGastos(){
-
-}
+    function anyadirGasto(gasto){
+        
+    }
     
-function calcularBalance(){
-
-}
+    function borrarGasto(id){
+        
+    }
+        
+    function calcularTotalGastos(){
     
+    }
+        
+    function calcularBalance(){
+    
+    }
+        
+    
+    // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
+    // Las funciones y objetos deben tener los nombres que se indican en el enunciado
+    // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+    
+    export {
+        mostrarPresupuesto,
+        actualizarPresupuesto,
+        CrearGasto,
+        listarGastos,
+        anyadirGasto,
+        borrarGasto,
+        calcularTotalGastos,
+        calcularBalance
+    }
+        
 
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+            
+            
+             
+            
+            
 
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto,
-    listarGastos,
-    anyadirGasto,
-    borrarGasto,
-    calcularTotalGastos,
-    calcularBalance
-}
+          
+            
 
